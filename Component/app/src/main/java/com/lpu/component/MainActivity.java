e87fdaa24ed908eb4c191829fc964ca9d0386f66
Shivam Singh <vishweshbajpai16@gmail.com>
2021-11-14 16:22:44 +0530

    first commit

---
+++ Component/app/src/main/java/com/lpu/component/MainActivity.java
@@ -0,0 +1,50 @@
+package com.lpu.component;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private RadioGroup radioSexGroup;
+    CheckBox checkBox;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        radioSexGroup=(RadioGroup)findViewById(R.id.radioGroup);
+        checkBox = findViewById(R.id.checkbox);
+
+        radioSexGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener()
+        {
+            public void onCheckedChanged(RadioGroup group, int checkedId)
+            {
+                RadioButton checkedRadioButton = (RadioButton)group.findViewById(checkedId);
+                boolean isChecked = checkedRadioButton.isChecked();
+                if (isChecked)
+                {
+                    Toast.makeText(getApplicationContext(), checkedRadioButton.getText(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView,boolean isChecked) {
+                if (isChecked)
+                {
+                    Toast.makeText(getApplicationContext(), checkBox.getText().toString(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+        );
+    }
+}
